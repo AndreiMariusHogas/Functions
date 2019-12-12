f6f6a2d2c23d29a0910d7a0c815c2d396b66131e
unknown <hogasandreimarius@gmail.com>
2019-12-13 00:05:52 +0100

    divisble Sum pairs solved

--- Hackerrank/hackerrankSolutions#1.js
+++ Hackerrank/hackerrankSolutions#1.js
@@ -311,10 +311,10 @@ birthday([1,2,1,3,2],3,2);
 //Divisible Sum Pairs
 function divisibleSumPairs(n, k, ar) {
     let pairsArr = [];
-    for(let j=0;j<n;j++){
-        for(let i=0;i<n;i++){
-            if(ar[j]<ar[i] && (ar[j] + ar[i]) % k === 0){
-                pairsArr.push([ar[j],ar[i]]);
+    for(let i=0; i < n - 1; i++){
+        for(let j = i + 1;j < n;j++){
+            if((ar[i]+ar[j]) % k === 0){
+                pairsArr.push([ar[i],ar[j]]);
             }
         }
     }
