1bb634ba07caf87463638ed997a7b60c595f8caf
unknown <hogasandreimarius@gmail.com>
2020-01-09 16:37:54 +0100

    Time in Words Solved

--- Hackerrank/hackerranksolutions#2.js
+++ Hackerrank/hackerranksolutions#2.js
@@ -1127,3 +1127,67 @@ howManyGames(20,3,6,85);
 howManyGames(16,2,1,9981);
 howManyGames(1,100,1,9777);
 //Works
+
+
+//The Time in Words
+//Write Object with time as letters
+//Check for input
+//Return Object keys that match the input
+
+function timeInWords(h, m) {
+    const objectText={
+        1:"one",
+        2:"two",
+        3:"three",
+        4:"four",
+        5:"five",
+        6:"six",
+        7:"seven",
+        8:"eight",
+        9:"nine",
+        10:"ten",
+        11:"eleven",
+        12:"twelve",
+        13:"thirteen",
+        14:"fourteen",
+        16:"sixteen",
+        17:"seventeen",
+        18:"eighteen",
+        19:"nineteen",
+        20:"twenty",
+        21:"twenty one",
+        22:"twenty two",
+        23:"twenty three",
+        24:"twenty four",
+        25:"twenty five",
+        26:"twenty six",
+        27:"twenty seven",
+        28:"twenty eight",
+        29:"twenty nine"
+    };
+    if(m==0){
+      return objectText[h]+" o' clock";
+    }
+    else if(m==1){
+      return objectText[m]+" minute past "+objectText[h];
+    }
+    else if(m>1 & m<15 | m>15 & m<30){
+      return objectText[m]+" minutes past "+objectText[h];
+    }
+    else if(m==15){
+      return "quarter past "+objectText[h];
+    }
+    else if(m==30){
+      return "half past "+objectText[h];
+    }
+    else if(m>30 & m<45 | m>45 & m<=60){
+      console.log("h=",h);
+      return objectText[60-m]+" minutes to "+ objectText[++h];
+    }
+    else if(m==45){
+      return "quarter to "+ objectText[++h];
+    }
+
+}
+timeInWords(5,47);
+
